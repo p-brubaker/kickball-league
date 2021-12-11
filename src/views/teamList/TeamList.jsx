6038968247a1c3a2af093a189e66e96e99cdea67
Paul Brubaker <pbrubak@gmail.com>
2021-12-11 13:04:44 -0800

    fix some async state management issues

--- src/views/teamList/TeamList.jsx
+++ src/views/teamList/TeamList.jsx
@@ -1,11 +1,11 @@
-import { getTeams, deleteTeamById } from '../../services/teams'
+import { getTeams, deleteTeamById, addTeam } from '../../services/teams'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './TeamList.css'
 
 export default function TeamList() {
     const [teams, setTeams] = useState([])
-    const [didDeleteToggle, setDidDeleteToggle] = useState(false)
+    const [deleted, setDeleted] = useState([])
 
     useEffect(() => {
         async function get() {
@@ -13,11 +13,16 @@ export default function TeamList() {
             setTeams(teams)
         }
         get()
-    }, [didDeleteToggle])
+    }, [deleted])
 
     async function handleDelete(id) {
-        await deleteTeamById(id)
-        setDidDeleteToggle(prev => !prev)
+        const res = await deleteTeamById(id)
+        setDeleted(prev => [...prev, {name: res[0].name, city: res[0].city, state: res[0].state }])
+    }
+
+    async function handleRestore(deletedTeam) {
+        await addTeam(deletedTeam)
+        setDeleted(prev => [...prev.filter(team => team.name !== deletedTeam.name)])
     }
 
     return (
@@ -25,12 +30,21 @@ export default function TeamList() {
             <h1>Teams</h1>
             <Link to='/teams/add'>Add a team</Link>
             {teams ? (
-                teams.map(team => (
-                <div className="team">
-                    <Link key={team.id} to={`/teams/view/${team.id}`}>{team.name}</Link>
-                    <button onClick={() => handleDelete(team.id)}>DELETE</button>
+                <div className="teams-list">
+                {teams.map(team => (
+                    <div className="team" key={team.id}>
+                        <Link to={`/teams/view/${team.id}`}>{team.name}</Link>
+                        <button onClick={() => handleDelete(team.id)}>DELETE</button>
+                        <Link to={`/teams/update/${team.id}`}>Update</Link>
+                    </div>
+                ))}
+                {deleted.map(deletedTeam => (
+                    <div key={deletedTeam.name} className="deleted-team">
+                        <p style={{textDecoration: 'line-through'}}>{deletedTeam.name}</p>
+                        <button onClick={() => handleRestore(deletedTeam)}>Restore</button>    
+                    </div>
+                ))}
                 </div>
-                ))
             ) : <p>Loading...</p>}
         </div>
     )
